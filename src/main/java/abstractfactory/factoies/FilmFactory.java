0e8f454a7f5e723d57a33ce5d7ee18e0f4d96f00
Yevgen Kharchenko <yevgen.kharchenko.jd@gmail.com>
2019-10-24 22:02:15 +0300

    movie servis

---
+++ src/main/java/abstractfactory/factoies/FilmFactory.java
@@ -0,0 +1,9 @@
+package abstractfactory.factoies;
+
+import abstractfactory.audio.Audio;
+import abstractfactory.subtitles.Subtitles;
+
+public interface FilmFactory {
+    Audio createAudio();
+    Subtitles createSubtitles();
+}
